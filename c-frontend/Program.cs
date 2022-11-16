8385763b31e68f46e1017bc25c40ff74b12443c8
PortalHUN <giotomi03@gmail.com>
2022-11-16 17:51:57 +0100

    Server implementation

--- c-frontend/Program.cs
+++ c-frontend/Program.cs
@@ -1,30 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
 using RestSharp;
 
 namespace c_frontend
 {
   internal class Program
   {
+    public static string configPath = "config.txt";
     static void Main(string[] args)
     {
-      string url = "http://localhost:3000/";
-      getAPI(url);
-
-      Console.ReadKey();
-    }
-
-    public static void getAPI(string url)
-    {
-      RestClient client = new RestClient();
-      RestRequest request = new RestRequest(url);
-      Console.WriteLine(client.Get(request).Content);
-    }
-
-    class Post
-    {
-      public string Title { get; set; }
-      public string Body { get; set; }
-      public int UserId { get; set; }
+      Config.CheckConfig(configPath);
     }
   }
 }
